6eca540b4a981000ea154727d761253c03f6675b
J-Meira <jonathanmeira@hotmail.com>
2025-03-10 00:08:32 -0300

    feat: organized program in a new app and refactor get indirectly connections to get connections of any level

--- Connection.cs
+++
@@ -1,9 +0,0 @@
-namespace MyTest
-{
-  public class Connection(int elementA, int elementB)
-  {
-    public int ElementA { get; set; } = elementA;
-    public int ElementB { get; set; } = elementB;
-  }
-
-}
